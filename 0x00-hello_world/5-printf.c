d54dc11baab57dcf48566e2d32e3c7c1ee7c3f92
ekenefidel <echukwurah99@gmail.com>
2022-04-08 18:04:11 -0700

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, ");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
