cc1127264f2f169fd0d0fff03d15b7c8b7e7127e
ekenefidel <echukwurah99@gmail.com>
2022-04-08 17:28:55 -0700

    completed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *  * main - Prints a string of letters
+
+ *  * Return: 0
+ */
+
+int main(void)
+{
+	char str1[] = "Programming is like building a multilingual puzzle";
+
+	puts(str1);
+
+	return (0);
+}
