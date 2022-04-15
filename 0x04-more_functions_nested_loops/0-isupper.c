859b392866dd5a31650a840783453b90fac2ca99
ekenefidel <echukwurah99@gmail.com>
2022-04-14 17:41:32 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * _isupper - check if character is uppercase
+ *
+ * @c: integer to check
+ * Return: 1 if upper and 0 otherwise
+ */
+
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
