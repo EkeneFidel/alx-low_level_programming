2a822e76ef24dca13fd114421f5caccaa8233ffe
ekenefidel <echukwurah99@gmail.com>
2022-04-21 18:24:30 -0700

    made corrections

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,18 +3,18 @@
  * puts2 - Prints out every other char of the string.
  * @str: input string to print.
  */
-void print_rev(char *s)
+void puts2(char *str)
 {
-	int i = 0;
+	int i;
+	int j;
 
-	while (s[i] != 0)
+	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		i++;
+		;
 	}
-	while (i > 0)
+	for (j = 0 ; j < i ; j = j + 2)
 	{
-		i--;
-		_putchar(s[i]);
+		_putchar(str[j]);
 	}
 	_putchar('\n');
 }
